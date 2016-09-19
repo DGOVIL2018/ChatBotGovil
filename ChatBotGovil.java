53e2d69d609537044366e6f8058938204d2a7610
DGOVIL2018 <DGOVIL2018@pingry.org>
2016-09-19 11:17:15 -0400

    Add files via upload

---
+++ ChatBotGovil.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class ChatBotGovil {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in); //construct a Scanner object
+		
+		String input = kb.nextLine();
+		System.out.println("Hi" + input + "!. How are you doing today?");
+		
+		while (True){
+			
+			if (input.equalsIgnoreCase != "bye")
+			{
+				System.out.println("Hey, want to discuss something?");
+			}
+			
+			else
+			{
+				
+				System.exit(0);
+				
+			}
+			
+	
+			 
+			
+			
+				
+				
+				
+				
+				
+				
+				
+			}
+		
+		
+		
+		
+		
+		
+		}
+		
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+}
