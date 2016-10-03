d5292e04d2c4738fdab9b4c5d1deb07190a47b98
DGOVIL2018 <DGOVIL2018@pingry.org>
2016-10-02 20:39:39 -0400

    Add files via upload

---
+++ ChatBotGovil.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class ChatBotGovil {
+	
+/* Created By: Dhruv Govil
+   Version#: 1
+   Talk to the computer. Enter bye or BYE to exit the ChatBot
+
+
+**/
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in); //construct a Scanner object
+		
+		String input = kb.nextLine();
+		System.out.println("Hi" + input + "!. How are you doing today?");
+		
+		while (True){
+			
+			if (input.equalsIgnoreCase != "bye") //if the user input does not equal to "bye or "BYE", keep talking
+			{
+				System.out.println("Hey, want to discuss something?");
+			}
+			
+			else
+			{
+				
+				System.exit(0); //exit the program
+				
+			}
+			
+	
+			 
+			
+			
+				
+				
+				
+				
+				
+				
+				
+			}
+		
+		
+		
+		
+		
+		
+		}
+		
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+}
